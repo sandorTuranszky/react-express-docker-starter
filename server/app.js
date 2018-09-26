cdcc69b3ed76992c60059ca329e60f03a550c5cc
sturanszky <sturanszky@kwiff-mm-sturanszky.local>
2018-09-26 22:46:11 +0200

    add trust proxy

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const helmet = require('helmet')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 
-app.use(logger('dev'));
+/**
+ * When running Express app behind a proxy we need to detect client IP address correctly.
+ * For NGINX the following must be configured 'proxy_set_header X-Forwarded-For $remote_addr;'
+ * @link http://expressjs.com/en/guide/behind-proxies.html
+ */
+app.set('trust proxy', true);
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Helmet helps you secure your Express apps by setting various HTTP headers.
-app.use(helmet())
+app.use(helmet());
 
 module.exports = app;
